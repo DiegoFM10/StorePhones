fdb10920af9b0674982e5742b03bfe85e68ce220
Jimmy P <jimmyporinsu@gmail.com>
2021-10-20 20:21:52 -0500

    Libraries __init__ views

---
+++ backend/authApp/views/__init__.py
@@ -0,0 +1,3 @@
+from .userCreateView   import UserCreateView
+from .userDetailView   import UserDetailView
+from .productView import ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
