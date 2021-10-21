2205a8bd423ad2b917f8b2a8817de12111e7b3d4
Jimmy P <jimmyporinsu@gmail.com>
2021-10-20 20:24:07 -0500

    productSerializer

--- backend/authApp/serializers/productSerializer.py
+++ backend/authApp/serializers/productSerializer.py
@@ -1,5 +1,4 @@
 from authApp.models.product import Product
-#from authApp.models.user     import User
 from rest_framework          import serializers
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -9,8 +8,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def to_representation(self, obj):
         product = Product.objects.get(id=obj.id)
-        #origin_account  = Account.objects.get(id=obj.origin_account_id)
-        #destiny_account = Account.objects.get(id=obj.destiny_account_id)
+        
         return {
             'id'     : product.id,
             'marca'  : product.marca,
